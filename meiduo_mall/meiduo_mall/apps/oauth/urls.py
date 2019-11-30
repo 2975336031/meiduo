cbaa994d4b0061b96388b72ca044408fd813eed6
python <python@qq.com>
2019-11-30 18:29:34 +0800

    OAuth2.0认证完成

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     # 拼接QQ登录url
     url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
+    url(r'^oauth_callback/$', views.QQAuthView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -3,6 +3,7 @@ from QQLoginTool.QQtool import OAuthQQ
 from django import http
 
 from meiduo_mall.utils.response_code import RETCODE
+from django.conf import settings
 
 
 class QQAuthURLView(View):
@@ -13,7 +14,11 @@ class QQAuthURLView(View):
 
         # 2. 创建OAuthQQ 对象
         # auth_tool = OAuthQQ(client_id='appid', client_secret='appkey', redirect_uri='回调地址', state='把它当成next')
-        auth_tool = OAuthQQ(client_id='101568493', client_secret='e85ad1fa847b5b79d07e40f8f876b211', redirect_uri='http://www.meiduo.site:8000/oauth_callback', state=next)
+        # auth_tool = OAuthQQ(client_id='101568493', client_secret='e85ad1fa847b5b79d07e40f8f876b211', redirect_uri='http://www.meiduo.site:8000/oauth_callback', state=next)
+        auth_tool = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                            client_secret=settings.QQ_CLIENT_SECRET,
+                            redirect_uri=settings.QQ_REDIRECT_URI,
+                            state=next)
 
         # 3. 调用OAuthQQ 里面的get_qq_url方法得到拼接好的QQ登录url
         login_url = auth_tool.get_qq_url()
@@ -22,3 +27,22 @@ class QQAuthURLView(View):
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'login_url': login_url})
 
 
+class QQAuthView(View):
+    """QQ登录成功回调处理"""
+    def get(self, request):
+        # 1. 获取查询参数中的code
+        code = request.GET.get('code')
+        # 2. 校验
+        if code is None:
+            return http.HttpResponseForbidden('缺少code')
+        # 3. 创建QQ登录工具对象
+        auth_tool = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                            client_secret=settings.QQ_CLIENT_SECRET,
+                            redirect_uri=settings.QQ_REDIRECT_URI)
+
+        # 4. 调用get_access_token
+        access_token = auth_tool.get_access_token(code)
+        #  调用get_openid
+        openid = auth_tool.get_open_id(access_token)
+        # 查询openid是否和美多中的user有关联
+        pass
