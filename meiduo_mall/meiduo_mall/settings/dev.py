c749cae1db36baf0803df33ff14938e3d58f119f
python <python@qq.com>
2019-11-30 09:01:01 +0800

    定义用户模型类及迁移

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -15,7 +15,7 @@ import os, sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-sys.path.insert(0, os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -194,3 +194,7 @@ LOGGING = {
         },
     }
 }
+
+
+# 修改Django认证模块的模型类
+AUTH_USER_MODEL = 'users.User'
